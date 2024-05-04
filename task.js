5b9f10dd6a14e03257e2bf2ce3af342556823807
MadGenius16 <aleksandrco777@gmail.com>
2024-05-04 17:07:25 +0300

    Update task.js

--- task.js
+++ task.js
@@ -15,3 +15,25 @@ console.log(book["genres"]); // ["historical prose", "adventure"]
 const propKey = "author";
 console.log(book.propKey); // undefined
 console.log(book[propKey]); // "Bernard Cornwell"
+
+
+
+
+book.rating = 9;
+book.isPublic = false;
+book.genres.push("drama");
+console.log(book.rating); // 9
+console.log(book.isPublic); // false
+console.log(book.genres); // ["historical prose", "adventure", "drama"]
+
+
+const keys = Object.keys(book);
+console.log(keys); // ["title", "author", "rating"]
+const values = Object.values(book);
+console.log(values); // ["The Last Kingdom", "Bernard Cornwell", 8.38]
+
+const numbers = [5,10,15,20,30];
+const filterNumbers = numbers.filter((number)=>number<20)
+const filterNumbers1 = numbers.filter((number)=>number>=20)
+console.log(filterNumbers);
+console.log(filterNumbers1);
