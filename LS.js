a266d83d7d42b84335a7d8ee92392124f298f890
MadGenius16 <aleksandrco777@gmail.com>
2024-07-29 13:21:14 +0300

    1

--- LS.js
+++ LS.js
@@ -1,65 +1,26 @@
 const STORAGE_KEY = 'feedback-msg';
 
-// const form = document.querySelector('.feedback-form');
-// const textarea = form.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+const name = form.querySelector('input')
 
-form.addEventListener('input', () => {
-  const formData = new FormData(form);
-  const name = formData.get('name');
-  const message = formData.get('message');
-  const data = { name, message };
-
-  saveToLS('name', name);
-  saveToLS('message', message);
-  saveToLS('userData', data);
-});
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const formData = new FormData(form);
-  const name = formData.get('name');
-  const message = formData.get('message');
-  const data = { name, message };
-
-  console.log(data);
-
-  form.reset();
-
-  localStorage.removeItem('name');
-  localStorage.removeItem('message');
-  localStorage.removeItem('userData');
-});
-
-//!===============================================================
-
-function saveToLS(key, value) {
-  const jsonData = JSON.stringify(value);
-  localStorage.setItem(key, jsonData);
+function formSubmitHandler(evt){
+  evt.preventDefault()
+  const text = textarea.value;
+  const name = form.elements.name.value
+  const data =JSON.stringify({text, name})
+  localStorage.setItem(STORAGE_KEY, data)
 }
 
-function loadFromLS(key) {
-  const json = localStorage.getItem(key);
-  try {
-    const data = JSON.parse(json);
-    return data;
-  } catch {
-    return json;
-  }
+form.addEventListener("submit", formSubmitHandler)
+
+const jsn = localStorage.getItem(STORAGE_KEY) ?? "";
+try {
+  const data = JSON.parse(jsn);
+console.log(data);
+textarea.value = data.text;
+form.elements.name.value = data.name
 }
-
-//!===============================================================
-// window.addEventListener('DOMContentLoaded', () => {
-//   const name = loadFromLS('name');
-//   const message = loadFromLS('message');
-
-//   form.elements.name.value = name || 'Anonymous';
-//   form.elements.message.value = message;
-// });
-
-window.addEventListener('DOMContentLoaded', () => {
-  const data = loadFromLS('userData');
-
-  form.elements.name.value = data?.name ?? 'Anonymous';
-  form.elements.message.value = data?.message ?? '';
-});
+catch {
+  console.log("Fill please all fields");
+}
